2079efe1f32977a4ee73dfcd7dc79dde0cd0384d
Narayan Das Nitol <nitol.aust.cse@gmail.com>
2020-12-26 18:16:56 +0600

    final

---
+++ MVC_CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CRUD.Models;
+
+namespace MVC_CRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (Database1Entities1 db = new Database1Entities1())
+            {
+                List<Employee> empList = db.Employees.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            if(id == 0)
+            {
+                return View(new Employee());
+            }
+            else
+            {
+                using (Database1Entities1 db = new Database1Entities1())
+                {
+                    return View(db.Employees.Where(x => x.Id == id).FirstOrDefault<Employee>());
+                }
+            }
+            
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            using(Database1Entities1 db = new Database1Entities1())
+            {
+                if (emp.Id == 0)
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }     
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using(Database1Entities1 db = new Database1Entities1())
+            {
+                Employee emp = db.Employees.Where(x => x.Id == id).FirstOrDefault<Employee>();
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ MVC_CRUD/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Cryptography;
+using MVC_CRUD.Models;
+
+
+namespace MVC_CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Database1Entities2 db = new Database1Entities2();
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Session["id"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (ModelState.IsValid)
+            {              
+
+                var data = db.admins.Where(s => s.username.Equals(username) && s.password.Equals(password)).ToList();
+                if (data.Count() > 0)
+                {
+                    return RedirectToAction("../Employee/Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+    
+
+    }
+}
